2bf904c5292e0f78a67ef73f1a7c096f85630589
Stijn Swaanen <swaanenstijn@gmail.com>
2018-08-16 16:17:36 +0200

    WID-158 tests for small multiple dates

---
+++ tests/Multiple/SmallMultipleHTMLFormatterTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace CultuurNet\CalendarSummaryV3\Tests\Multiple;
+
+use CultuurNet\CalendarSummaryV3\Multiple\SmallMultipleHTMLFormatter;
+use CultuurNet\SearchV3\ValueObjects\Event;
+
+class SmallMultipleHTMLFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SmallMultipleHTMLFormatter
+     */
+    protected $formatter;
+
+    public function setUp()
+    {
+        $this->formatter = new SmallMultipleHTMLFormatter('nl_NL', false);
+    }
+
+    public function testFormatMultipleWithoutLeadingZeroes() {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('25-11-2025'));
+        $offer->setEndDate(new \DateTime('30-11-2030'));
+
+        $this->assertEquals(
+            '<span class="cf-from cf-meta">Van</span> <span class="cf-date">25 november 2025</span> '
+            . '<span class="cf-to cf-meta">tot</span> <span class="cf-date">30 november 2030</span>',
+            $this->formatter->format($offer)
+        );
+    }
+
+    public function testFormatMultipleWithLeadingZeroes()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('04-03-2025'));
+        $offer->setEndDate(new \DateTime('08-03-2030'));
+
+        $this->assertEquals(
+            '<span class="cf-from cf-meta">Van</span> <span class="cf-date">4 maart 2025</span> '
+            . '<span class="cf-to cf-meta">tot</span> <span class="cf-date">8 maart 2030</span>',
+            $this->formatter->format($offer)
+        );
+    }
+
+    public function testFormatMultipleMonthWithoutLeadingZero()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('04-10-2025'));
+        $offer->setEndDate(new \DateTime('08-10-2030'));
+
+        $this->assertEquals(
+            '<span class="cf-from cf-meta">Van</span> <span class="cf-date">4 oktober 2025</span> '
+            . '<span class="cf-to cf-meta">tot</span> <span class="cf-date">8 oktober 2030</span>',
+            $this->formatter->format($offer)
+        );
+    }
+
+    public function testFormatAMultipleWithSameBeginAndEndDate()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('08-10-2025'));
+        $offer->setEndDate(new \DateTime('08-10-2025'));
+
+        $output = '<span class="cf-weekday cf-meta">woensdag</span>';
+        $output .= ' ';
+        $output .= '<span class="cf-date">8 oktober 2025</span>';
+
+        $this->assertEquals(
+            $output,
+            $this->formatter->format($offer)
+        );
+    }
+}
---
+++ tests/Multiple/SmallMultiplePlainTextFormatterTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace CultuurNet\CalendarSummaryV3\Tests\Multiple;
+
+use CultuurNet\CalendarSummaryV3\Multiple\SmallMultiplePlainTextFormatter;
+use CultuurNet\SearchV3\ValueObjects\Event;
+
+class SmallMultiplePlainTextFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SmallMultiplePlainTextFormatter
+     */
+    protected $formatter;
+
+    public function setUp()
+    {
+        $this->formatter = new SmallMultiplePlainTextFormatter('nl_NL', false);
+    }
+
+    public function testFormatMultipleWithoutLeadingZeroes()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('25-11-2025'));
+        $offer->setEndDate(new \DateTime('30-11-2030'));
+
+        $this->assertEquals(
+            'Van 25 november 2025 tot 30 november 2030',
+            $this->formatter->format($offer)
+        );
+    }
+
+    public function testFormatMultipleWithLeadingZeroes()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('04-03-2025'));
+        $offer->setEndDate(new \DateTime('08-03-2030'));
+
+        $this->assertEquals(
+            'Van 4 maart 2025 tot 8 maart 2030',
+            $this->formatter->format($offer)
+        );
+    }
+
+
+    public function testFormatMultipleDayWithoutLeadingZero()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('25-03-2025'));
+        $offer->setEndDate(new \DateTime('30-03-2030'));
+
+        $this->assertEquals(
+            'Van 25 maart 2025 tot 30 maart 2030',
+            $this->formatter->format($offer)
+        );
+    }
+
+    public function testFormatMultipleMonthWithoutLeadingZero()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('04-10-2025'));
+        $offer->setEndDate(new \DateTime('08-10-2030'));
+
+        $this->assertEquals(
+            'Van 4 oktober 2025 tot 8 oktober 2030',
+            $this->formatter->format($offer)
+        );
+    }
+
+    public function testFormatAPeriodWithSameBeginAndEndDate()
+    {
+        $offer = new Event();
+        $offer->setStartDate(new \DateTime('08-10-2025'));
+        $offer->setEndDate(new \DateTime('08-10-2025'));
+
+        $this->assertEquals(
+            'woensdag 8 oktober 2025',
+            $this->formatter->format($offer)
+        );
+    }
+}
