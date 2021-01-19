b40140763465fa822de7e1c57ed460e84180a738
Bert Ramakers <bertramakers@me.com>
2021-01-19 18:31:21 +0100

    Remove abstract class LargeMultipleFormatter

--- src/Multiple/LargeMultiplePlainTextFormatter.php
+++ src/Multiple/LargeMultiplePlainTextFormatter.php
@@ -5,8 +5,24 @@ namespace CultuurNet\CalendarSummaryV3\Multiple;
 use CultuurNet\SearchV3\ValueObjects\Event;
 use CultuurNet\CalendarSummaryV3\Single\LargeSinglePlainTextFormatter;
 
-class LargeMultiplePlainTextFormatter extends LargeMultipleFormatter implements MultipleFormatterInterface
+class LargeMultiplePlainTextFormatter implements MultipleFormatterInterface
 {
+    /**
+     * @var string $langCode
+     */
+    protected $langCode;
+
+    /**
+     * @var bool $hidePast
+     */
+    protected $hidePast;
+
+    public function __construct(string $langCode, bool $hidePastDates)
+    {
+        $this->langCode = $langCode;
+        $this->hidePast = $hidePastDates;
+    }
+
     public function format(Event $event): string
     {
         $subEvents = $event->getSubEvents();
