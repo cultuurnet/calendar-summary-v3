7d0bb86a4e0955669576101b0b48aff3ccdf3a78
Jonas Drieghe <jonasd@madewithlove.be>
2021-01-20 19:58:40 +0100

    Depend on DateTime instead of DateTimeInterface
    The interface does not have setTimezone defined

--- src/DateComparison.php
+++ src/DateComparison.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace CultuurNet\CalendarSummaryV3;
 
+use DateTime;
 use DateTimeImmutable;
-use DateTimeInterface;
 use DateTimeZone;
 
 final class DateComparison
 {
-    public static function onSameDay(DateTimeInterface $date1, DateTimeInterface  $date2): bool
+    public static function onSameDay(DateTime $date1, DateTime  $date2): bool
     {
         $date1 = $date1->setTimezone(new DateTimeZone(date_default_timezone_get()));
         $date2 = $date2->setTimezone(new DateTimeZone(date_default_timezone_get()));
@@ -18,7 +18,7 @@ final class DateComparison
         return $date1->format('Y-m-d') === $date2->format('Y-m-d');
     }
 
-    public static function inTheFuture(DateTimeInterface $date): bool
+    public static function inTheFuture(DateTime $date): bool
     {
         $date = $date->setTimezone(new DateTimeZone(date_default_timezone_get()));
         $now = new DateTimeImmutable();
