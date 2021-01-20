4eb108b3fd7cfd42e641c7523aae7ff74f4ab62e
Luc Wollants <lucwollants@gmail.com>
2021-01-20 09:18:22 +0100

    Use ExtraSmallMultipleInformation inside HTML

--- src/Multiple/ExtraSmallMultipleHTMLFormatter.php
+++ src/Multiple/ExtraSmallMultipleHTMLFormatter.php
@@ -3,22 +3,20 @@
 namespace CultuurNet\CalendarSummaryV3\Multiple;
 
 use CultuurNet\SearchV3\ValueObjects\Event;
-use DateTimeZone;
 
 class ExtraSmallMultipleHTMLFormatter extends ExtraSmallMultipleFormatter implements MultipleFormatterInterface
 {
     public function format(Event $event): string
     {
-        $dateFrom = $event->getStartDate()->setTimezone(new DateTimeZone(date_default_timezone_get()));
-        $dateTo = $event->getEndDate()->setTimezone(new DateTimeZone(date_default_timezone_get()));
+        $info = new ExtraSmallMultipleInformation($event);
 
-        if ($dateFrom == $dateTo) {
-            return '<span class="cf-date">' . $dateFrom->format('j/n/y') . '</span>';
+        if ($info->getFrom() === $info->getTo()) {
+            return '<span class="cf-date">' . $info->getFrom() . '</span>';
         }
 
         return '<span class="cf-from cf-meta">' . ucfirst($this->trans->getTranslations()->t('from')) . '</span> ' .
-            '<span class="cf-date">' . $dateFrom->format('j/n/y') . '</span> ' .
+            '<span class="cf-date">' . $info->getFrom() . '</span> ' .
             '<span class="cf-to cf-meta">' . $this->trans->getTranslations()->t('till') . '</span> '.
-            '<span class="cf-date">' . $dateTo->format('j/n/y') . '</span>';
+            '<span class="cf-date">' . $info->getTo() . '</span>';
     }
 }
