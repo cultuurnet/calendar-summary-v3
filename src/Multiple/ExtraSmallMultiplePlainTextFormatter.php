bddaed4e6d874fe1e059ad8c76b45f3e509133b3
Luc Wollants <lucwollants@gmail.com>
2021-01-20 09:18:32 +0100

    Use ExtraSmallMultipleInformation inside Plain text

--- src/Multiple/ExtraSmallMultiplePlainTextFormatter.php
+++ src/Multiple/ExtraSmallMultiplePlainTextFormatter.php
@@ -3,22 +3,20 @@
 namespace CultuurNet\CalendarSummaryV3\Multiple;
 
 use CultuurNet\SearchV3\ValueObjects\Event;
-use DateTimeZone;
 
 class ExtraSmallMultiplePlainTextFormatter extends ExtraSmallMultipleFormatter implements MultipleFormatterInterface
 {
     public function format(Event $event): string
     {
-        $dateFrom = $event->getStartDate()->setTimezone(new DateTimeZone(date_default_timezone_get()));
-        $dateTo = $event->getEndDate()->setTimezone(new DateTimeZone(date_default_timezone_get()));
+        $info = new ExtraSmallMultipleInformation($event);
 
-        if ($dateFrom == $dateTo) {
-            return $dateFrom->format('j/n/y');
+        if ($info->getFrom() === $info->getTo()) {
+            return $info->getFrom();
         }
 
         return ucfirst($this->trans->getTranslations()->t('from')) . ' ' .
-            $dateFrom->format('j/n/y') . ' ' .
+            $info->getFrom() . ' ' .
             $this->trans->getTranslations()->t('till') . ' '.
-            $dateTo->format('j/n/y');
+            $info->getTo();
     }
 }
