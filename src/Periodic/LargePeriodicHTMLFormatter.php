ba18aaeec517a3c203e5567d4b25e470f9d5a434
Bert Ramakers <bertramakers@me.com>
2021-01-20 18:07:37 +0100

    Fix casing in large periodic html formatter

--- src/Periodic/LargePeriodicHTMLFormatter.php
+++ src/Periodic/LargePeriodicHTMLFormatter.php
@@ -136,7 +136,7 @@ final class LargePeriodicHTMLFormatter implements PeriodicFormatterInterface
      */
     private function generateWeekScheme($openingHoursData)
     {
-        $outputWeek = '<p class="cf-openinghours">' . $this->trans->getTranslations()->t('open') . ':</p>';
+        $outputWeek = '<p class="cf-openinghours">' . ucfirst($this->trans->getTranslations()->t('open')) . ':</p>';
         $outputWeek .= '<ul class="list-unstyled">';
 
         // Create an array with formatted timespans.
