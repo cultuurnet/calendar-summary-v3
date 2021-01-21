ed177f6b4b6e1d7fafb3483981e37d28eb653563
Bert Ramakers <bertramakers@me.com>
2021-01-21 14:00:15 +0100

    Use PlainTextSummaryBuilder in LargeSinglePlainTextFormatter

--- src/Single/LargeSinglePlainTextFormatter.php
+++ src/Single/LargeSinglePlainTextFormatter.php
@@ -4,6 +4,7 @@ namespace CultuurNet\CalendarSummaryV3\Single;
 
 use CultuurNet\CalendarSummaryV3\DateComparison;
 use CultuurNet\CalendarSummaryV3\DateFormatter;
+use CultuurNet\CalendarSummaryV3\PlainTextSummaryBuilder;
 use CultuurNet\CalendarSummaryV3\Translator;
 use CultuurNet\SearchV3\ValueObjects\Offer;
 use DateTimeInterface;
@@ -49,19 +50,26 @@ final class LargeSinglePlainTextFormatter implements SingleFormatterInterface
         $intlStartTimeFrom = $this->formatter->formatAsTime($dateFrom);
         $intlEndTimeEnd = $this->formatter->formatAsTime($dateEnd);
 
+        $summaryBuilder = (new PlainTextSummaryBuilder($this->trans))
+            ->add($intlWeekDayFrom)
+            ->add($intlDateFrom);
+
         if ($intlStartTimeFrom === '00:00' && $intlEndTimeEnd === '23:59') {
-            return ucfirst($intlWeekDayFrom) . ' ' . $intlDateFrom;
+            return $summaryBuilder->toString();
         }
 
-        if ($intlStartTimeFrom == $intlEndTimeEnd) {
-            return ucfirst($intlWeekDayFrom) . ' ' . $intlDateFrom . ' '
-                . $this->trans->getTranslations()->t('at') . ' ' . $intlStartTimeFrom;
+        if ($intlStartTimeFrom === $intlEndTimeEnd) {
+            return $summaryBuilder
+                ->addTranslation('at')
+                ->add($intlStartTimeFrom)
+                ->toString();
         }
 
-        return ucfirst($intlWeekDayFrom) . ' ' . $intlDateFrom
-            . ' ' . $this->trans->getTranslations()->t('from') . ' '
-            . $intlStartTimeFrom
-            . ' ' . $this->trans->getTranslations()->t('till') . ' ' . $intlEndTimeEnd;
+        return $summaryBuilder
+            ->addTranslation('from')
+            ->add($intlStartTimeFrom)
+            ->addTranslation('till')
+            ->add($intlEndTimeEnd);
     }
 
     private function formatMoreDays(DateTimeInterface $dateFrom, DateTimeInterface $dateEnd): string
@@ -74,15 +82,16 @@ final class LargeSinglePlainTextFormatter implements SingleFormatterInterface
         $intlWeekDayEnd = $this->formatter->formatAsDayOfWeek($dateEnd);
         $intlEndTimeEnd = $this->formatter->formatAsTime($dateEnd);
 
-        return ucfirst(
-            $this->trans->getTranslations()->t('from') . ' '
-            . $intlWeekDayFrom . ' ' . $intlDateFrom . ' '
-            . $this->trans->getTranslations()->t('at') . ' ' . $intlStartTimeFrom . ' '
-            . $this->trans->getTranslations()->t('till') . ' '
-            . $intlWeekDayEnd . ' ' . $intlDateEnd . ' '
-            . $this->trans->getTranslations()->t('at') . ' ' . $intlEndTimeEnd
-        );
-
-        return $output;
+        return (new PlainTextSummaryBuilder($this->trans))
+            ->addTranslation('from')
+            ->add($intlWeekDayFrom)
+            ->add($intlDateFrom)
+            ->addTranslation('at')
+            ->add($intlStartTimeFrom)
+            ->addTranslation('till')
+            ->add($intlWeekDayEnd)
+            ->add($intlDateEnd)
+            ->addTranslation('at')
+            ->add($intlEndTimeEnd);
     }
 }
