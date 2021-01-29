06544c9b8253a7cf91a2a87fa6e270f9cde65f89
Jonas Drieghe <jonasd@madewithlove.be>
2021-01-29 07:33:28 +0100

    Add tests to verify MediumSinglePlainTextFormatterTest appends status

--- tests/Single/MediumSinglePlainTextFormatterTest.php
+++ tests/Single/MediumSinglePlainTextFormatterTest.php
@@ -4,6 +4,7 @@ namespace CultuurNet\CalendarSummaryV3\Single;
 
 use CultuurNet\CalendarSummaryV3\Translator;
 use CultuurNet\SearchV3\ValueObjects\Event;
+use CultuurNet\SearchV3\ValueObjects\Status;
 use PHPUnit\Framework\TestCase;
 
 class MediumSinglePlainTextFormatterTest extends TestCase
@@ -21,6 +22,7 @@ class MediumSinglePlainTextFormatterTest extends TestCase
     public function testFormatHTMLSingleDateMediumOneDay(): void
     {
         $event = new Event();
+        $event->setStatus(new Status('Available'));
         $event->setStartDate(new \DateTime('2018-01-25T20:00:00+01:00'));
         $event->setEndDate(new \DateTime('2018-01-25T21:30:00+01:00'));
 
@@ -33,6 +35,7 @@ class MediumSinglePlainTextFormatterTest extends TestCase
     public function testFormatHTMLSingleDateMediumWithLeadingZeroOneDay(): void
     {
         $event = new Event();
+        $event->setStatus(new Status('Available'));
         $event->setStartDate(new \DateTime('2018-01-08T20:00:00+01:00'));
         $event->setEndDate(new \DateTime('2018-01-08T21:30:00+01:00'));
 
@@ -45,6 +48,7 @@ class MediumSinglePlainTextFormatterTest extends TestCase
     public function testFormatHTMLSingleDateMediumMoreDays(): void
     {
         $event = new Event();
+        $event->setStatus(new Status('Available'));
         $event->setStartDate(new \DateTime('2018-01-25T20:00:00+01:00'));
         $event->setEndDate(new \DateTime('2018-01-27T21:30:00+01:00'));
 
@@ -57,11 +61,38 @@ class MediumSinglePlainTextFormatterTest extends TestCase
     public function testFormatHTMLSingleDateMediumWithLeadingZeroMoreDays(): void
     {
         $event = new Event();
+        $event->setStatus(new Status('Available'));
         $event->setStartDate(new \DateTime('2018-01-06T20:00:00+01:00'));
         $event->setEndDate(new \DateTime('2018-01-08T21:30:00+01:00'));
 
         $this->assertEquals(
             'Van zaterdag 6 januari 2018 tot maandag 8 januari 2018',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateMediumOneDayWithUnavailableStatus(): void
+    {
+        $event = new Event();
+        $event->setStatus(new Status('Unavailable'));
+        $event->setStartDate(new \DateTime('2018-01-25T20:00:00+01:00'));
+        $event->setEndDate(new \DateTime('2018-01-25T21:30:00+01:00'));
+
+        $this->assertEquals(
+            'Donderdag 25 januari 2018 (geannuleerd)',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateMediumOneDayWithTemporarilyUnavailableStatus(): void
+    {
+        $event = new Event();
+        $event->setStatus(new Status('TemporarilyUnavailable'));
+        $event->setStartDate(new \DateTime('2018-01-25T20:00:00+01:00'));
+        $event->setEndDate(new \DateTime('2018-01-25T21:30:00+01:00'));
+
+        $this->assertEquals(
+            'Donderdag 25 januari 2018 (uitgesteld)',
             $this->formatter->format($event)
         );
     }
