89a2623b5bcd5a913e306c279717dd51bf47acb8
Jonas Drieghe <jonasd@madewithlove.be>
2021-01-29 08:06:04 +0100

    Add additional test case to verify status rendering of single SubEvent

--- tests/Multiple/MediumMultiplePlainTextFormatterTest.php
+++ tests/Multiple/MediumMultiplePlainTextFormatterTest.php
@@ -96,4 +96,33 @@ class MediumMultiplePlainTextFormatterTest extends TestCase
             $this->formatter->format($event)
         );
     }
+
+    public function testFormatPlainTextMultipleDateMediumOneDayWithUnavailableStatusForSingleSubEvent(): void
+    {
+        $subEvents = json_decode(file_get_contents(__DIR__ . '/data/subEvents.json'), true);
+        $event = new Event();
+        $event->setStatus(new Status('Available'));
+        $newEvents = array();
+        foreach ($subEvents as $subEvent) {
+            $e = new Event();
+            $e->setStatus(new Status('Available'));
+            $e->setStartDate(new \DateTime($subEvent['startDate']));
+            $e->setEndDate(new \DateTime($subEvent['endDate']));
+            $newEvents[] = $e;
+        }
+
+        $newEvents[1]->setStatus(new Status('Unavailable'));
+
+        $event->setSubEvents($newEvents);
+
+        $expectedOutput = 'Donderdag 9 november 2017'. PHP_EOL;
+        $expectedOutput .= 'Donderdag 16 november 2017 (geannuleerd)' . PHP_EOL;
+        $expectedOutput .= 'Donderdag 23 november 2017' . PHP_EOL;
+        $expectedOutput .= 'Donderdag 30 november 2017';
+
+        $this->assertEquals(
+            $expectedOutput,
+            $this->formatter->format($event)
+        );
+    }
 }
