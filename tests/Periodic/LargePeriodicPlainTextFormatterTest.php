e8c98fdc5d7ca0edb1f57d1e9ef076879c097e40
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-01 13:37:17 +0100

    Remove pointless docblock

--- tests/Periodic/LargePeriodicPlainTextFormatterTest.php
+++ tests/Periodic/LargePeriodicPlainTextFormatterTest.php
@@ -7,10 +7,6 @@ use CultuurNet\SearchV3\ValueObjects\OpeningHours;
 use CultuurNet\SearchV3\ValueObjects\Place;
 use PHPUnit\Framework\TestCase;
 
-/**
- * Provide unit tests for large plain text periodic formatter.
- * @package CultuurNet\CalendarSummaryV3\Periodic
- */
 class LargePeriodicPlainTextFormatterTest extends TestCase
 {
     /**
