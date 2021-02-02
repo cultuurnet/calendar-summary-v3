4570b238858dfe7f2b03e43d3ac3cf581def0ebb
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-02 09:51:41 +0100

    Add HtmlStatusFormatter to allow reuse across formatters

---
+++ src/HtmlStatusFormatter.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\CalendarSummaryV3;
+
+use CultuurNet\SearchV3\ValueObjects\Offer;
+use CultuurNet\SearchV3\ValueObjects\Place;
+use CultuurNet\SearchV3\ValueObjects\Status;
+
+class HtmlStatusFormatter
+{
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    /**
+     * @var Status
+     */
+    private $status;
+
+    /**
+     * @var string
+     */
+    private $element = 'span';
+
+    /**
+     * @var bool
+     */
+    private $withBraces = false;
+
+    /**
+     * @var bool
+     */
+    private $capitalize = false;
+
+    /**
+     * @var bool
+     */
+    private $isPlace = false;
+
+    private function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public static function forOffer(Offer $offer, Translator $translator): self
+    {
+        $formatter = new self($translator);
+        $formatter->status = $offer->getStatus();
+        $formatter->isPlace = $offer instanceof Place;
+        return $formatter;
+    }
+
+    public function withBraces(): self
+    {
+        $c = clone $this;
+        $c->withBraces = true;
+        return $c;
+    }
+
+    public function withoutBraces(): self
+    {
+        $c = clone $this;
+        $c->withBraces = false;
+        return $c;
+    }
+
+    public function withElement(string $element): self
+    {
+        $c = clone $this;
+        $c->element = $element;
+        return $c;
+    }
+
+    public function capitalize(): self
+    {
+        $c = clone $this;
+        $c->capitalize = true;
+        return $c;
+    }
+
+    public function toString(): string
+    {
+        if ($this->status->getType() === 'Available') {
+            return '';
+        }
+
+        $openTag = '<' . $this->element . ' ' . $this->getReasonAsTitleAttribute() . 'class="cf-status">';
+        $closingTag = '</' . $this->element . '>';
+        $statusText = $this->getStatusText();
+
+        if ($this->capitalize) {
+            $statusText = ucfirst($statusText);
+        }
+
+        if ($this->withBraces) {
+            $statusText = '(' . $statusText . ')';
+        }
+
+        return $openTag . $statusText . $closingTag;
+    }
+
+    private function getStatusText(): string
+    {
+        if ($this->isPlace) {
+            return $this->status->getType() === 'Unavailable' ?
+                $this->translator->translate('permanently_closed') :
+                $this->translator->translate('temporarily_closed');
+        }
+
+        return $this->status->getType() === 'Unavailable' ?
+            $this->translator->translate('cancelled') :
+            $this->translator->translate('postponed');
+    }
+
+    private function getReasonAsTitleAttribute(): string
+    {
+        $reason = $this->status->getReason();
+
+        if (!$reason) {
+            return '';
+        }
+
+        $translatedReason = $reason->getValueForLanguage($this->translator->getLanguageCode());
+
+        if (empty($translatedReason)) {
+            return '';
+        }
+
+        return 'title="' . $translatedReason . '" ';
+    }
+}
--- src/Middleware/NonAvailablePlaceHTMLFormatter.php
+++ src/Middleware/NonAvailablePlaceHTMLFormatter.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace CultuurNet\CalendarSummaryV3\Middleware;
 
 use Closure;
-use CultuurNet\CalendarSummaryV3\TranslatedStatusReasonFormatter;
+use CultuurNet\CalendarSummaryV3\HtmlStatusFormatter;
 use CultuurNet\CalendarSummaryV3\Translator;
 use CultuurNet\SearchV3\ValueObjects\Offer;
 use CultuurNet\SearchV3\ValueObjects\Place;
@@ -27,14 +27,11 @@ class NonAvailablePlaceHTMLFormatter implements FormatterMiddleware
     public function format(Offer $offer, Closure $next): string
     {
         if ($this->appliesToOffer($offer)) {
-            $statusText = $status = $offer->getStatus()->getType() === 'Unavailable' ?
-                $this->translator->translate('permanently_closed') :
-                $this->translator->translate('temporarily_closed');
-
-            $reasonFormatter = new TranslatedStatusReasonFormatter($this->translator);
-            $titleAttribute = $reasonFormatter->formatAsTitleAttribute($offer->getStatus());
-
-            return '<span ' . $titleAttribute . 'class="cf-status">' . $statusText . '</span>';
+            return HtmlStatusFormatter::forOffer($offer, $this->translator)
+                ->withElement('span')
+                ->withoutBraces()
+                ->capitalize()
+                ->toString();
         }
 
         return $next($offer);
