325b5d06ace449b5c68cbcadc83b3fae561846de
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-02 08:21:41 +0100

    Remove unnecessary private methods

--- src/Periodic/ExtraSmallPeriodicPlainTextFormatter.php
+++ src/Periodic/ExtraSmallPeriodicPlainTextFormatter.php
@@ -7,8 +7,6 @@ use CultuurNet\CalendarSummaryV3\DateFormatter;
 use CultuurNet\CalendarSummaryV3\PlainTextSummaryBuilder;
 use CultuurNet\CalendarSummaryV3\Translator;
 use CultuurNet\SearchV3\ValueObjects\Offer;
-use \DateTime;
-use \DateTimeInterface;
 
 final class ExtraSmallPeriodicPlainTextFormatter implements PeriodicFormatterInterface
 {
@@ -34,22 +32,16 @@ final class ExtraSmallPeriodicPlainTextFormatter implements PeriodicFormatterInt
         $startDate->setTime(0, 0, 1);
 
         if (DateComparison::inTheFuture($startDate)) {
-            return $this->formatNotStarted($startDate)->appendStatus($offer->getStatus())->toString();
+            return PlainTextSummaryBuilder::start($this->translator)
+                ->fromPeriod($this->formatter->formatAsShortDate($startDate))
+                ->appendStatus($offer->getStatus())
+                ->toString();
         }
 
         $endDate = $offer->getEndDate();
-        return $this->formatStarted($endDate)->appendStatus($offer->getStatus())->toString();
-    }
-
-    private function formatStarted(DateTimeInterface $endDate): PlainTextSummaryBuilder
-    {
         return PlainTextSummaryBuilder::start($this->translator)
-            ->till($this->formatter->formatAsShortDate($endDate));
-    }
-
-    private function formatNotStarted(DateTimeInterface $startDate): PlainTextSummaryBuilder
-    {
-        return PlainTextSummaryBuilder::start($this->translator)
-            ->fromPeriod($this->formatter->formatAsShortDate($startDate));
+            ->till($this->formatter->formatAsShortDate($endDate))
+            ->appendStatus($offer->getStatus())
+            ->toString();
     }
 }
