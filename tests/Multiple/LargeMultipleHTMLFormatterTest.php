bd791fce490df4f759f931ef69df8df721441139
Jonas Drieghe <jonasd@madewithlove.be>
2021-02-02 13:56:52 +0100

    Append status to LargeMultipleHTMLFormatterTest
    This was already working because it relies on a previously changed formatter

--- tests/Multiple/LargeMultipleHTMLFormatterTest.php
+++ tests/Multiple/LargeMultipleHTMLFormatterTest.php
@@ -103,6 +103,91 @@ class LargeMultipleHTMLFormatterTest extends TestCase
         );
     }
 
+    public function testFormatHTMLMultipleDateLargeOneDayWithUnavailableStatus(): void
+    {
+        $subEvents = json_decode(file_get_contents(__DIR__ . '/data/subEvents.json'), true);
+        $event = new Event();
+        $event->setStatus(new Status('Available'));
+        $newEvents = array();
+        foreach ($subEvents as $subEvent) {
+            $e = new Event();
+            $e->setStatus(new Status('Available'));
+            $e->setStartDate(new \DateTime($subEvent['startDate']));
+            $e->setEndDate(new \DateTime($subEvent['endDate']));
+            $newEvents[] = $e;
+        }
+
+        $newEvents[1]->setStatus(new Status('Unavailable'));
+        $event->setSubEvents($newEvents);
+
+        $expectedOutput = '<ul class="cnw-event-date-info"><li>';
+        $expectedOutput .= '<time itemprop="startDate" datetime="2017-11-09T20:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-weekday cf-meta">Donderdag</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">9 november 2017</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-from cf-meta">van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-time">20:00</span>';
+        $expectedOutput .= '</time>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<time itemprop="endDate" datetime="2017-11-09T22:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-time">22:00</span>';
+        $expectedOutput .= '</time></li>';
+        $expectedOutput .= '<li><time itemprop="startDate" datetime="2017-11-16T20:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-weekday cf-meta">Donderdag</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">16 november 2017</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-from cf-meta">van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-time">20:00</span>';
+        $expectedOutput .= '</time>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<time itemprop="endDate" datetime="2017-11-16T22:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-time">22:00</span>';
+        $expectedOutput .= '</time> <span class="cf-status">(geannuleerd)</span></li>';
+        $expectedOutput .= '<li><time itemprop="startDate" datetime="2017-11-23T20:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-weekday cf-meta">Donderdag</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">23 november 2017</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-from cf-meta">van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-time">20:00</span>';
+        $expectedOutput .= '</time>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<time itemprop="endDate" datetime="2017-11-23T22:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-time">22:00</span>';
+        $expectedOutput .= '</time></li>';
+        $expectedOutput .= '<li><time itemprop="startDate" datetime="2017-11-30T20:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-weekday cf-meta">Donderdag</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">30 november 2017</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-from cf-meta">van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-time">20:00</span>';
+        $expectedOutput .= '</time>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<time itemprop="endDate" datetime="2017-11-30T22:00:00+01:00">';
+        $expectedOutput .= '<span class="cf-time">22:00</span>';
+        $expectedOutput .= '</time></li></ul>';
+
+        $this->assertEquals(
+            $expectedOutput,
+            $this->formatter->format($event)
+        );
+    }
+
     public function testFormatHTMLMultipleDateLargeMoreDays(): void
     {
         $subEvents = json_decode(file_get_contents(__DIR__ . '/data/subEventsMoreDays.json'), true);
