60b589679bd839d625ae2eb644feb8b7979cb61a
Jonas Drieghe <jonasd@madewithlove.be>
2021-03-08 11:19:14 +0100

    Expect OfferType to be created from context parameter

---
+++ tests/Offer/OfferTypeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\CalendarSummaryV3\Offer;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+final class OfferTypeTest extends TestCase
+{
+    public function it_can_create_place_offer_type_from_context(): void
+    {
+        $this->assertEquals(
+            OfferType::fromContext('/contexts/place'),
+            OfferType::place()
+        );
+    }
+
+    public function it_can_create_event_offer_type_from_context(): void
+    {
+        $this->assertEquals(
+            OfferType::fromContext('/contexts/event'),
+            OfferType::event()
+        );
+    }
+
+    public function it_cannot_create_offer_type_from_invalid_context(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->assertEquals(
+            OfferType::fromContext('/contexts/NOT_A_VALID_CONTEXT'),
+            OfferType::event()
+        );
+    }
+}
