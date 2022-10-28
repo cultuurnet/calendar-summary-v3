a80decdcd5bcd37d8393c0f4f62b18c8e01cace6
Jonas <jonas@cultuurnet.be>
2022-10-28 16:33:50 +0200

    Add Extra DateComparisons

--- src/DateComparison.php
+++ src/DateComparison.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CultuurNet\CalendarSummaryV3;
 
+use DateInterval;
 use DateTimeImmutable;
 use DateTimeZone;
 
@@ -15,6 +16,29 @@ final class DateComparison
         $date2 = $date2->setTimezone(new DateTimeZone(date_default_timezone_get()));
 
         return $date1->format('Y-m-d') === $date2->format('Y-m-d');
+    }
+
+    public static function isThisEvening(DateTimeImmutable $date): bool
+    {
+        return  self::onSameDay($date, new DateTimeImmutable()) && (int)$date->format('G') >= 18;
+    }
+
+    public static function isToday(DateTimeImmutable $date): bool
+    {
+        return self::onSameDay($date, new DateTimeImmutable());
+    }
+
+    public static function isTomorrow(DateTimeImmutable $date): bool
+    {
+        return self::onSameDay($date, (new DateTimeImmutable())->add(new DateInterval('P1D')));
+    }
+
+    public static function inTheSameYear(DateTimeImmutable $date1, DateTimeImmutable  $date2): bool
+    {
+        $date1 = $date1->setTimezone(new DateTimeZone(date_default_timezone_get()));
+        $date2 = $date2->setTimezone(new DateTimeZone(date_default_timezone_get()));
+
+        return $date1->format('Y') === $date2->format('Y');
     }
 
     public static function inTheFuture(DateTimeImmutable $date): bool
