8a8696bf6228b32665785c42c3277aac9dc43f14
Jonas <jonas@cultuurnet.be>
2022-11-15 11:18:23 +0100

    Add tests for relative formats for SmallSingleHTMLFormatter

--- tests/Single/SmallSingleHTMLFormatterTest.php
+++ tests/Single/SmallSingleHTMLFormatterTest.php
@@ -9,6 +9,7 @@ use CultuurNet\CalendarSummaryV3\Offer\Offer;
 use CultuurNet\CalendarSummaryV3\Offer\OfferType;
 use CultuurNet\CalendarSummaryV3\Offer\Status;
 use CultuurNet\CalendarSummaryV3\Translator;
+use DateInterval;
 use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
@@ -36,6 +37,100 @@ final class SmallSingleHTMLFormatterTest extends TestCase
 
         $this->assertEquals(
             '<span class="cf-date">25</span> <span class="cf-month">jan</span> <span class="cf-year">2018</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHtmlSingleDateSmOneDayCurrentYear(): void
+    {
+        $someDayInCurrentYear =
+            (new DateTimeImmutable())->add(new DateInterval('P10D'))->format('Y') === (new DateTimeImmutable())->format('Y')
+                ? (new DateTimeImmutable())->add(new DateInterval('P10D')) : (new DateTimeImmutable())->sub(new DateInterval('P10D'));
+        // This is to catch edge cases when running the tests at the end of or beginning of the current year
+        $expected = '<span class="cf-date">' .
+            $someDayInCurrentYear->format('d') .
+            '</span> <span class="cf-month">' .
+            strtolower($someDayInCurrentYear->format('M')) .
+            '</span>';
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable($someDayInCurrentYear->format('Y-m-d') . 'T20:00:00+01:00'),
+            new DateTimeImmutable($someDayInCurrentYear->format('Y-m-d') . 'T21:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            $expected,
+            $this->formatter->format($event)
+        );
+    }
+
+
+    public function testFormatHTMLSingleDateXsToday(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable((new DateTimeImmutable())->format('Y-m-d') . 'T11:00:00+01:00'),
+            new DateTimeImmutable((new DateTimeImmutable())->format('Y-m-d') . 'T20:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-days">Vandaag</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsTonight(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable((new DateTimeImmutable())->format('Y-m-d') . 'T18:00:00+01:00'),
+            new DateTimeImmutable((new DateTimeImmutable())->format('Y-m-d') . 'T20:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-days">Vanavond</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsTomorrow(): void
+    {
+        $tomorrow = (new DateTimeImmutable())->add(new DateInterval('P1D'));
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable($tomorrow->format('Y-m-d') . 'T18:30:00+01:00'),
+            new DateTimeImmutable($tomorrow->format('Y-m-d') . 'T22:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-days">Morgen</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatPlainTextSingleCurrentWeek(): void
+    {
+        $offSet = (int) (new DateTimeImmutable())->format('w');
+        $daysTillSunday = 7 - $offSet;
+        $thisSunday = (new DateTimeImmutable())->add(new DateInterval('P' . $daysTillSunday . 'D'));
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable($thisSunday->format('Y-m-d') . 'T18:30:00+01:00'),
+            new DateTimeImmutable($thisSunday->format('Y-m-d') . 'T22:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-meta">Deze</span> <span class="cf-days">zondag</span>',
             $this->formatter->format($event)
         );
     }
