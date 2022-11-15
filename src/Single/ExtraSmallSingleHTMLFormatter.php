873f887227f59dd9097fb5a63d951bf82becd3d2
Jonas <jonas@cultuurnet.be>
2022-11-15 10:52:32 +0100

    split up small & extrasmall html formatter

---
+++ src/Single/ExtraSmallSingleHTMLFormatter.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\CalendarSummaryV3\Single;
+
+use CultuurNet\CalendarSummaryV3\DateComparison;
+use CultuurNet\CalendarSummaryV3\DateFormatter;
+use CultuurNet\CalendarSummaryV3\HtmlAvailabilityFormatter;
+use CultuurNet\CalendarSummaryV3\Translator;
+use CultuurNet\CalendarSummaryV3\Offer\Offer;
+use DateTimeInterface;
+
+final class ExtraSmallSingleHTMLFormatter implements SingleFormatterInterface
+{
+    /**
+     * @var DateFormatter
+     */
+    private $formatter;
+
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    public function __construct(Translator $translator)
+    {
+        $this->formatter = new DateFormatter($translator->getLocale());
+        $this->translator = $translator;
+    }
+
+    public function format(Offer $offer): string
+    {
+        $dateFrom = $offer->getStartDate();
+        $dateEnd = $offer->getEndDate();
+
+        if (DateComparison::onSameDay($dateFrom, $dateEnd)) {
+            $output = $this->formatSameDay($dateFrom);
+        } else {
+            $output = $this->formatMoreDays($dateFrom, $dateEnd);
+        }
+
+        $optionalAvailability = HtmlAvailabilityFormatter::forOffer($offer, $this->translator)
+            ->withBraces()
+            ->toString();
+
+        return trim($output . ' ' . $optionalAvailability);
+    }
+
+    private function formatSameDay(DateTimeInterface $dateFrom): string
+    {
+        $dateFromDay = $this->formatter->formatAsDayNumber($dateFrom);
+        $dateFromMonth = $this->formatter->formatAsAbbreviatedMonthName($dateFrom);
+
+        return '<span class="cf-date">' . ucfirst($dateFromDay) . '</span>'
+            . ' '
+            . '<span class="cf-month">' . $dateFromMonth . '</span>';
+    }
+
+    private function formatMoreDays(DateTimeInterface $dateFrom, DateTimeInterface $dateEnd): string
+    {
+        $dateFromDay = $this->formatter->formatAsDayNumber($dateFrom);
+        $dateFromMonth = $this->formatter->formatAsAbbreviatedMonthName($dateFrom);
+
+        $dateEndDay = $this->formatter->formatAsDayNumber($dateEnd);
+        $dateEndMonth = $this->formatter->formatAsAbbreviatedMonthName($dateEnd);
+
+        $output = '<span class="cf-from cf-meta">' . ucfirst($this->translator->translate('from')) . '</span>';
+        $output .= ' ';
+        $output .= '<span class="cf-date">' . $dateFromDay . '</span>';
+        $output .= ' ';
+        $output .= '<span class="cf-month">' . $dateFromMonth . '</span>';
+        $output .= ' ';
+        $output .= '<span class="cf-to cf-meta">' . $this->translator->translate('till') . '</span>';
+        $output .= ' ';
+        $output .= '<span class="cf-date">' . $dateEndDay . '</span>';
+        $output .= ' ';
+        $output .= '<span class="cf-month">' . $dateEndMonth . '</span>';
+
+        return $output;
+    }
+}
---
+++ tests/Single/ExtraSmallSingleHTMLFormatterTest.php
@@ -0,0 +1,176 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\CalendarSummaryV3\Single;
+
+use CultuurNet\CalendarSummaryV3\Offer\BookingAvailability;
+use CultuurNet\CalendarSummaryV3\Offer\Offer;
+use CultuurNet\CalendarSummaryV3\Offer\OfferType;
+use CultuurNet\CalendarSummaryV3\Offer\Status;
+use CultuurNet\CalendarSummaryV3\Translator;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+final class ExtraSmallSingleHTMLFormatterTest extends TestCase
+{
+    /**
+     * @var SmallSingleHTMLFormatter
+     */
+    protected $formatter;
+
+    protected function setUp(): void
+    {
+        $this->formatter = new ExtraSmallSingleHTMLFormatter(new Translator('nl_NL'));
+    }
+
+    public function testFormatHTMLSingleDateXsOneDay(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-25T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-25T21:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-date">25</span> <span class="cf-month">jan</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsOneDayWithStatusUnavailable(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Unavailable', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-25T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-25T21:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-date">25</span> <span class="cf-month">jan</span> <span class="cf-status">(geannuleerd)</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsOneDayWithStatusUnavailableAndReason(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Unavailable', ['nl' => 'Covid-19']),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-25T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-25T21:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-date">25</span> <span class="cf-month">jan</span> <span title="Covid-19" class="cf-status">(geannuleerd)</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsWithLeadingZeroOneDay(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-08T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-08T21:30:00+01:00')
+        );
+
+        $this->assertEquals(
+            '<span class="cf-date">8</span> <span class="cf-month">jan</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsMoreDays(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-25T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-27T21:30:00+01:00')
+        );
+
+        $expectedOutput = '<span class="cf-from cf-meta">Van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">25</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-month">jan</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">27</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-month">jan</span>';
+
+        $this->assertEquals(
+            $expectedOutput,
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsMoreDaysWithUnavailableStatus(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Unavailable', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-25T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-27T21:30:00+01:00')
+        );
+
+        $expectedOutput = '<span class="cf-from cf-meta">Van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">25</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-month">jan</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">27</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-month">jan</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-status">(geannuleerd)</span>';
+
+        $this->assertEquals(
+            $expectedOutput,
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHTMLSingleDateXsWithLeadingZeroMoreDays(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            new DateTimeImmutable('2018-01-06T20:00:00+01:00'),
+            new DateTimeImmutable('2018-01-08T21:30:00+01:00')
+        );
+
+        $expectedOutput = '<span class="cf-from cf-meta">Van</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">6</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-month">jan</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-to cf-meta">tot</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-date">8</span>';
+        $expectedOutput .= ' ';
+        $expectedOutput .= '<span class="cf-month">jan</span>';
+
+        $this->assertEquals(
+            $expectedOutput,
+            $this->formatter->format($event)
+        );
+    }
+}
