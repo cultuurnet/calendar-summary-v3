c0ef9e3105974cad00ccc414e2be2b6763c263f5
Jonas <jonas@cultuurnet.be>
2022-11-17 09:17:07 +0100

    Implement trait for RelativeDateHTML

---
+++ src/RelativeDateHTMLFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\CalendarSummaryV3;
+
+use DateTimeInterface;
+
+trait RelativeDateHTMLFormatter
+{
+    public function getRelativeDate(DateTimeInterface $date, Translator $translator, DateFormatter $formatter): ?string
+    {
+        if (DateComparison::isThisEvening($date)) {
+            return '<span class="cf-days">' . $translator->translate('tonight') . '</span>';
+        }
+        if (DateComparison::isToday($date)) {
+            return '<span class="cf-days">' . $translator->translate('today') . '</span>';
+        }
+        if (DateComparison::isTomorrow($date)) {
+            return '<span class="cf-days">' . $translator->translate('tomorrow') . '</span>';
+        }
+        if (DateComparison::isCurrentWeek($date)) {
+            return '<span class="cf-meta">' .
+                $translator->translate('this') .
+                '</span>' .
+                ' ' .
+                '<span class="cf-days">' .
+                $formatter->formatAsDayOfWeek($date) .
+                '</span>';
+        }
+        return null;
+    }
+}
