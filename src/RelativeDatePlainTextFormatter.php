b18b8fc4c6e52f611975392e8d5848d478c2e1e7
Jonas <jonas@cultuurnet.be>
2022-11-17 09:07:00 +0100

    Make trait for RelativeDatePlainTextFormatter

---
+++ src/RelativeDatePlainTextFormatter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\CalendarSummaryV3;
+
+use DateTimeInterface;
+
+trait RelativeDatePlainTextFormatter
+{
+    public function getRelativeDate(DateTimeInterface $date, Translator $translator, DateFormatter $formatter): ?string
+    {
+        if (DateComparison::isThisEvening($date)) {
+            return $translator->translate('tonight');
+        }
+        if (DateComparison::isToday($date)) {
+            return $translator->translate('today');
+        }
+        if (DateComparison::isTomorrow($date)) {
+            return $translator->translate('tomorrow');
+        }
+        if (DateComparison::isCurrentWeek($date)) {
+            $preposition = $translator->translate('this');
+            $weekDay = $formatter->formatAsDayOfWeek($date);
+            return PlainTextSummaryBuilder::singleLine($preposition, $weekDay);
+        }
+        return null;
+    }
+}
