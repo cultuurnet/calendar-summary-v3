8c538131e057cc42a9042ac4d93f4b813ca13caa
JonasVHG <jonas@cultuurnet.be>
2022-11-23 15:06:39 +0100

    Add extra tests for current year-htmlFormatter

--- tests/Single/ExtraSmallSingleHTMLFormatterTest.php
+++ tests/Single/ExtraSmallSingleHTMLFormatterTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CultuurNet\CalendarSummaryV3\Single;
 
+use Carbon\CarbonImmutable;
 use CultuurNet\CalendarSummaryV3\Offer\BookingAvailability;
 use CultuurNet\CalendarSummaryV3\Offer\Offer;
 use CultuurNet\CalendarSummaryV3\Offer\OfferType;
@@ -22,6 +23,7 @@ final class ExtraSmallSingleHTMLFormatterTest extends TestCase
     protected function setUp(): void
     {
         $this->formatter = new ExtraSmallSingleHTMLFormatter(new Translator('nl_NL'));
+        CarbonImmutable::setTestNow(CarbonImmutable::create(2021, 5, 9));
     }
 
     public function testFormatHTMLSingleDateXsOneDay(): void
@@ -57,6 +59,87 @@ final class ExtraSmallSingleHTMLFormatterTest extends TestCase
             '<span class="cf-month">jan</span> ' .
             '<span class="cf-year">2018</span> ' .
             '<span class="cf-status">(geannuleerd)</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHtmlSingleDateCurrentYEar(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            CarbonImmutable::create(2021, 1, 8)->setTime(1, 0),
+            CarbonImmutable::create(2021, 1, 8)->setTime(21, 30)
+        );
+
+        $this->assertEquals(
+            '<span class="cf-date">8</span> <span class="cf-month">jan</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHtmlSingleDateXsMoreDaysCurrentYear(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            CarbonImmutable::create(2021, 1, 23)->setTime(1, 0),
+            CarbonImmutable::create(2021, 1, 28)->setTime(21, 30)
+        );
+
+        $this->assertEquals(
+            '<span class="cf-from cf-meta">Van</span> ' .
+            '<span class="cf-date">23</span> ' .
+            '<span class="cf-month">jan</span> ' .
+            '<span class="cf-to cf-meta">tot</span> ' .
+            '<span class="cf-date">28</span> ' .
+            '<span class="cf-month">jan</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHtmlSingleDateXsMoreDaysStartCurrentYear(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            CarbonImmutable::create(2021, 12, 23)->setTime(1, 0),
+            CarbonImmutable::create(2022, 1, 28)->setTime(21, 30)
+        );
+
+        $this->assertEquals(
+            '<span class="cf-from cf-meta">Van</span> ' .
+            '<span class="cf-date">23</span> ' .
+            '<span class="cf-month">dec</span> ' .
+            '<span class="cf-to cf-meta">tot</span> ' .
+            '<span class="cf-date">28</span> ' .
+            '<span class="cf-month">jan</span> ' .
+            '<span class="cf-year">2022</span>',
+            $this->formatter->format($event)
+        );
+    }
+
+    public function testFormatHtmlSingleDateXsMoreDaysEndCurrentYear(): void
+    {
+        $event = new Offer(
+            OfferType::event(),
+            new Status('Available', []),
+            new BookingAvailability('Available'),
+            CarbonImmutable::create(2020, 12, 23)->setTime(1, 0),
+            CarbonImmutable::create(2021, 1, 28)->setTime(21, 30)
+        );
+
+        $this->assertEquals(
+            '<span class="cf-from cf-meta">Van</span> ' .
+            '<span class="cf-date">23</span> ' .
+            '<span class="cf-month">dec</span> ' .
+            '<span class="cf-year">2020</span> ' .
+            '<span class="cf-to cf-meta">tot</span> ' .
+            '<span class="cf-date">28</span> ' .
+            '<span class="cf-month">jan</span>',
             $this->formatter->format($event)
         );
     }
