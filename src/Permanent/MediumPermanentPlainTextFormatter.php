18673a0356d236f209c2ae49fc9bfd7885c8ca8c
JonasVHG <jonas@cultuurnet.be>
2022-11-30 13:01:21 +0100

    implement isFirstPeriodASingleDay() in PlainTextFormatter

--- src/Permanent/MediumPermanentPlainTextFormatter.php
+++ src/Permanent/MediumPermanentPlainTextFormatter.php
@@ -83,16 +83,16 @@ final class MediumPermanentPlainTextFormatter implements PermanentFormatterInter
 
         $weekScheme = $this->getWeekScheme($weekDaysOpen, $this->formatter);
 
-        $isFirstPeriodASingleDay = array_pop($weekScheme);
-
         // Put all the day names with opening hours on a single line with 'Open at' (sec) at the beginning.
-        // E.g. 'Open at mo - th & su'
-        if ($isFirstPeriodASingleDay) {
+        // E.g. 'Open at mo & th - su'
+        if ($this->isFirstPeriodASingleDay($weekDaysOpen)) {
             return PlainTextSummaryBuilder::start($this->translator)
                 ->openAt(...$weekScheme)
                 ->toString();
         }
 
+        // Put all the day names with opening hours on a single line with 'Open at' (sec) at the beginning.
+        // E.g. 'Open at mo - th & su'
         return PlainTextSummaryBuilder::start($this->translator)
             ->openFrom(...$weekScheme)
             ->toString();
